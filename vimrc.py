b390850a84c6206b565e175a12fb9789f09ed0ad
JmilkFan <fanguiju1992@163.com>
2021-11-26 17:39:10 +0000

    edit vimrc.py

--- vimrc.py
+++ vimrc.py
@@ -10,7 +10,12 @@ set noerrorbells                          " No noise
 set confirm                               " 存在 .swp 文件时需要确认
 set nofoldenable                          " 不启用折叠
 set hlsearch                              " 高亮搜索关键字
-set ignorecase                            " 搜索/替换时大小写敏感
+set noignorecase                          " 搜索/替换时大小写敏感
+set splitright                            " 指定窗口分割时把新窗口放到当前窗口之右
+nmap <leader>v :vsplit<CR>
+set number                                " 显示行号
+set numberwidth=4                         " 指定行号宽度
+set list listchars=tab:>.,trail:.         " 显示 tab、空格的制表符
 
 " 编码选项
 set fileencodings=utf-8,gb2312,gbk
@@ -18,23 +23,6 @@ set fileencoding=utf-8
 set termencoding=utf-8
 set encoding=utf-8
 set fileformat=unix
-
-" 布局选项
-set number                                " 显示行号
-set numberwidth=4                         " 指定行号宽度
-set textwidth=79                          " 指定 text 宽度，PEP8 为 79
-set colorcolumn=79                        " 指定宽度高亮
-set splitright                            " 指定窗口分割时把新窗口放到当前窗口之右
-set list listchars=tab:>.,trail:.         " 显示 tab、空格的制表符
-
-" 缩进选项
-set tabstop=4                             " 指定 tap 宽度
-set expandtab                             " 用空格代替 tab
-set shiftwidth=4                          " 指定制表符的宽度
-set softtabstop=4                         " 指定自动缩进的宽度
-set showmatch                             " 显示匹配的括号
-set autoindent
-set smartindent
 
 " 在 insert mode 下开启光标线和光标柱，普通模式时关闭
 autocmd InsertLeave * call ToggleCursor()
@@ -44,25 +32,46 @@ function ToggleCursor() abort
     set cursorcolumn!
 endfunction
 
+" Python 支持
+au BufNewFile,BufRead *.py...
+\ set tabstop=4                             " 指定 tap 宽度
+\ set softtabstop=4                         " 指定自动缩进的宽度
+\ set shiftwidth=4                          " 指定制表符的宽度
+\ set textwidth=79                          " 指定 text 宽度
+\ set colorcolumn=79                        " 指定宽度高亮
+\ set expandtab                             " 用空格代替 tab
+\ set showmatch                             " 显示匹配的括号
+\ set autoindent
+\ set smartindent
+
+" 设置 git commit message 文本宽度及英文拼写检查
+autocmd Filetype gitcommit setlocal spell textwidth=72
+
+
 """""""""""""""""""""""""""""""""""""""" Vundle Plugins
 set rtp+=~/.vim/bundle/Vundle.vim
 call vundle#begin()
 Plugin 'VundleVim/Vundle.vim'
+
 Plugin 'altercation/vim-colors-solarized'
 Plugin 'altercation/solarized'
 Plugin 'powerline/powerline', {'rtp': 'powerline/bindings/vim'}
-Plugin 'scrooloose/nerdtree'
+
+Plugin 'preservim/nerdtree'
 Plugin 'jistr/vim-nerdtree-tabs'
 Plugin 'Xuyuanp/nerdtree-git-plugin'
-Plugin 'preservim/tagbar'
+Plugin 'ryanoasis/vim-devicons'
+"Plugin 'tiagofumo/vim-nerdtree-syntax-highlight'                  " 卡顿，慎用
+
 Plugin 'Valloric/YouCompleteMe'
 Plugin 'python-mode/python-mode'
+
+Plugin 'preservim/tagbar'
 Plugin 'easymotion/vim-easymotion'
 call vundle#end()
 
 
 " solarized
-colorscheme solarized
 set t_Co=256
 set background=dark
 let g:solarized_termcolors=16
@@ -80,15 +89,30 @@ set statusline+=%F
 
 
 " nerdtree
-let NERDChristmasTree=0
-let NERDTreeWinSize=35
-let NERDTreeChDirMode=2
-let NERDTreeIgnore=['\~$', '\.pyc$', '\.swp$', '^__pycache__$']
-let NERDTreeShowBookmarks=1
-let NERDTreeWinPos="left"
-autocmd vimenter * if !argc() | NERDTree | endif
-autocmd bufenter * if (winnr("$") == 1 && exists("b:NERDTreeType") && b:NERDTreeType == "primary") | q | endif
+"autocmd VimEnter * NERDTree                                        " 自动开启
+let NERDTreeChDirMode=2                                            " Change current working directory based on root directory in NERDTree
+let g:NERDTreeHidden=0                                             " 不显示隐藏文件
+let NERDTreeIgnore=['\~$', '\.pyc$', '\.swp$', '^__pycache__$']    " 过滤文件类型
+let NERDTreeWinSize=35                                             " Initial NERDTree width
+let NERDTreeWinPos="left"                                          " 左侧窗口
+let NERDTreeShowBookmarks=1                                        " 开启 Nerdtree 时自动显示书签
+let NERDChristmasTree=1
+autocmd vimenter * if !argc() | NERDTree | endif                   " 打开 vim 时如果没有文件则自动打开 NERDTree
+autocmd bufenter * if (winnr("$") == 1 && exists("b:NERDTreeType") && b:NERDTreeType == "primary") | q | endif  " 当 NERDTree 为剩下的唯一窗口时自动关闭
 nmap <leader>n :NERDTreeToggle<CR>
+let g:NERDTreeGitStatusUseNerdFonts = 1
+let g:NERDTreeGitStatusShowIgnored = 1
+let g:NERDTreeGitStatusIndicatorMapCustom = {
+    \ "Modified"  : "✹",
+    \ "Staged"    : "✚",
+    \ "Untracked" : "✭",
+    \ "Renamed"   : "➜",
+    \ "Unmerged"  : "═",
+    \ "Deleted"   : "✖",
+    \ "Dirty"     : "✗",
+    \ "Clean"     : "✔︎",
+    \ "Unknown"   : "?"
+    \ }
 
 
 " tagbar
@@ -127,22 +151,24 @@ let g:pymode_syntax_indent_errors = g:pymode_syntax_all
 let g:pymode_syntax_space_errors = g:pymode_syntax_all
 let g:pymode_breakpoint = 1               " 使用 python-mode 设置断点
 let g:pymode_breakpoint_bind = '<leader>b'
-let g:pymode_rope_goto_definition_bind = '<C-s>' " 显示 function/method defines
+"let g:pymode_rope_goto_definition_bind = '<C-f>' " 显示 function/method defines
 let g:pymode_lint = 1                     " 使用 lint 语法检查
-let g:pymode_lint_on_write = 1
-let g:pymode_lint_on_fly = 0              " 关闭编辑时进行检查
+"let g:pymode_lint_checkers = ['pylint']   " 默认使用 pyflakes，pylint 会更加严格
+let g:pymode_lint_on_write = 1            " :w 关闭编辑时进行检查
+let g:pymode_lint_on_fly = 0              " 关闭编写时进行检查
 let g:pymode_lint_signs = 1               " 显示 python-mode 相关的标志
-let g:pymode_lint_todo_symbol = 'WW'
-let g:pymode_lint_comment_symbol = 'CC'
-let g:pymode_lint_visual_symbol = 'RR'
-let g:pymode_lint_error_symbol = 'EE'
-let g:pymode_lint_info_symbol = 'II'
 let g:pymode_lint_pyflakes_symbol = 'FF'
-let g:pymode_lint_checkers = ['pylint', 'pep8']
+let g:pymode_lint_error_symbol = 'EE'
+let g:pymode_lint_todo_symbol = 'WW'
+let g:pymode_lint_visual_symbol = 'RR'
+let g:pymode_lint_comment_symbol = 'CC'
+let g:pymode_lint_info_symbol = 'II'
 let g:pymode_virtualenv = 1               " 使用 virtualenv
 let g:pymode_motion = 1                   " 使用快捷移动
 let g:pymode_trim_whitespaces = 1         " 保存文件时自动删除无用空格
-let g:pymode_warnings = 0                 " 开启警告
+let g:pymode_warnings = 1                 " 开启警告
+nmap <leader>8 :PymodeLintAuto<CR>
+nmap <leader>l :PymodeLint<CR>
 
 
 " easymotion
@@ -152,3 +178,8 @@ map <Leader><Leader>k <Plug>(easymotion-j)
 map <Leader><Leader>j <Plug>(easymotion-k)
 map <Leader><leader>l <Plug>(easymotion-lineforward)
 map <Leader><leader>. <Plug>(easymotion-repeat)
+
+" ctags setting
+set tags+=/root/workspace/tf-api-client/build/debug/api-lib/tags
+set tags+=/root/workspace/tf-controller/src/config/device-manager/tags
+set tags+=/root/workspace/dci-controller/tags
--- zshrc
+++ zshrc
@@ -6,11 +6,22 @@ export ZSH="/root/.oh-my-zsh"
 
 ZSH_THEME="powerlevel10k/powerlevel10k"
 
-plugins=(git)
-
 source $ZSH/oh-my-zsh.sh
 
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 
 unsetopt inc_append_history
 unsetopt share_history
+
+export CC=/usr/local/gcc-9.1.0/bin/gcc
+export CXX=/usr/local/gcc-9.1.0/bin/g++
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/gcc-9.1.0/lib64
+export PATH=$PATH:/usr/local/vim/bin
+export PATH=$PATH:/usr/local/zsh/bin
+
+alias vi="/usr/local/vim/bin/vim"
+
+alias proxy="export https_proxy=http://192.168.159.1:7890 http_proxy=http://192.168.159.1:7890 all_proxy=socks5://192.168.159.1:7890; curl google.com"
+alias gs="git status"
+alias gd="git diff"
+alias codelines="git ls-files | xargs cat | wc -l"
